dee57a2d30be9624c3ad64e2fceb260e7ac715e2
Helio <hslima@gmail.com>
2017-05-13 10:51:37 -0300

    C20170513-1000

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# config
+# git config
+
+git clone URL
+git init
+git add .
+git commit -m "Commit title"
+git remote add origin URL
+git remote -v
+git push origin master
+
---
+++ bin/config-server.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+config=/opt/zfolder/config
+
+cd $config
+
+rm -f  ~/.bashrc
+rm -f  ~/.tmux.conf
+rm -f  ~/.vimrc
+rm -rf ~/.vim
+
+ln -s $config/bashrc ~/.bashrc
+ln -s $config/tmux-server.conf ~/.tmux.conf
+ln -s $config/vim ~/.vim
+ln -s $config/vim/vimrc ~/.vimrc
+
